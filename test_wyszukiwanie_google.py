23fed818a9f6c964371f881188b76ee882b79756
Tmotoja <suwak-obwinienie.9n@icloud.com>
2025-03-23 09:50:44 +0100

    update

--- test_wyszukiwanie_google.py
+++ test_wyszukiwanie_google.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 import pytest
+from selenium.webdriver.common import by
 
 @pytest.fixture
 def driver():
@@ -38,3 +39,5 @@ def test_wyszukiwanie(driver):
 
     assert len(results) > 0, "brak wynikow"
     driver.quit()  # zamkniecie przegladarki
+
+
